1a35f490535d5473898da21272ec627be84f2659
Sandun Prasanna <sandunpmapa@gmail.com>
2023-07-06 21:15:42 +0530

    :zap: Update with Parsley validations

--- views/auth/login.php
+++ views/auth/login.php
@@ -4,23 +4,24 @@
     <div class="wrapper">
         <section class="form login">
             <header class="mb-4 text-center">REALTIME Chat App</header>
-            <form action="#" method="POST" enctype="multipart/form-data" autocomplete="off">
+            <form action="#" method="POST" enctype="multipart/form-data" autocomplete="off" id="loginForm" data-parsley-validate>
                 <div class="error-text"></div>
                 <div class="mb-3">
                     <label for="email" class="form-label">Email Address</label>
-                    <input type="text" name="email" id="email" class="form-control" placeholder="Enter your email">
+                    <input type="text" name="email" id="email" class="form-control" data-parsley-type="email" placeholder="Enter your email" required data-parsley-required-message="Please enter an Email address">
                 </div>
                 <div class="mb-3">
                     <label for="password" class="form-label">Password</label>
                     <div class="input-group">
-                        <input type="password" name="password" id="password" class="form-control" placeholder="Enter your password">
+                        <input type="password" name="password" id="password" class="form-control" data-parsley-minlength="6" data-parsley-errors-container=".error" placeholder="Enter your password" required data-parsley-required-message="Please enter a Password">
                         <button class="btn btn-outline-secondary" type="button" id="togglePassword">
                             <i class="fas fa-eye"></i>
                         </button>
                     </div>
+                    <div class="error"></div>
                 </div>
                 <div class="field button">
-                    <button class="btn btn-dark p-2">Continue to Chat</button>
+                    <button class="btn btn-dark p-2" id="login">Continue to Chat</button>
                 </div>
             </form>
             <div class="link">Not yet signed up? <a href="<?php echo $routeToRegister ?>">Signup now</a></div>
@@ -28,8 +29,22 @@
     </div>
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="/mvc architecture/asserts/javascript/parsley.min.js"></script>
+
     <script>
         $(document).ready(function() {
+
+            $("#loginForm").parsley({
+                errorClass: "is-invalid text-danger",
+                successClass: "is-valid",
+                errorsWrapper: '<span class="form-group"></span>',
+                errorTemplate: '<small class="form-text text-danger"></small>'
+            });
+
+            $('form').submit(function(e) {
+                e.preventDefault();
+            });
+
             $('#togglePassword').click(function() {
                 var passwordInput = $('#password');
                 var passwordFieldType = passwordInput.attr('type');
@@ -43,33 +58,36 @@
                 }
             });
 
-            $('form').submit(function(event) {
-                var errors = [];
 
-                validateField('email', errors);
-                validateField('password', errors);
+            /* 
+                        $('form').submit(function(event) {
+                            var errors = [];
 
-                if (errors.length > 0) {
-                    var errorText = errors.join('<br>');
-                    $('.error-text').html(errorText).show();
-                    event.preventDefault();
-                }
-            });
+                            validateField('email', errors);
+                            validateField('password', errors);
 
-            function validateField(fieldName, errors) {
-                var fieldInput = $('input[name="' + fieldName + '"]');
-                var errorText = fieldInput.siblings('.error-text');
+                            if (errors.length > 0) {
+                                var errorText = errors.join('<br>');
+                                $('.error-text').html(errorText).show();
+                                event.preventDefault();
+                            }
+                        });
 
-                if (fieldInput.val().trim() === '') {
-                    errors.push(capitalizeFirstLetter(fieldName) + ' is empty!');
-                }
+                        function validateField(fieldName, errors) {
+                            var fieldInput = $('input[name="' + fieldName + '"]');
+                            var errorText = fieldInput.siblings('.error-text');
 
-                errorText.hide();
-            }
+                            if (fieldInput.val().trim() === '') {
+                                errors.push(capitalizeFirstLetter(fieldName) + ' is empty!');
+                            }
 
-            function capitalizeFirstLetter(string) {
-                return string.charAt(0).toUpperCase() + string.slice(1);
-            }
+                            errorText.hide();
+                        }
+
+                        function capitalizeFirstLetter(string) {
+                            return string.charAt(0).toUpperCase() + string.slice(1);
+                        }
+                         */
         });
     </script>
 
--- views/auth/register.php
+++ views/auth/register.php
@@ -4,50 +4,50 @@
     <div class="wrapper">
         <section class="form signup">
             <header class="mb-4 text-center">REALTIME Chat App</header>
-            <form action="#" method="POST" enctype="multipart/form-data" autocomplete="off">
+            <form action="#" method="POST" enctype="multipart/form-data" autocomplete="off" id="registerForm" data-parsley-validate>
                 <div class="error-text mb-3"></div>
                 <div class="row mb-3">
                     <div class="col">
                         <div class="field input">
                             <label class="form-label">First Name</label>
-                            <input type="text" name="fname" class="form-control" placeholder="First name" required>
+                            <input type="text" name="fname" class="form-control" placeholder="First name" required data-parsley-required-message="Please enter a Last Name">
                         </div>
                     </div>
                     <div class="col">
                         <div class="field input">
                             <label class="form-label">Last Name</label>
-                            <input type="text" name="lname" class="form-control" placeholder="Last name" required>
+                            <input type="text" name="lname" class="form-control" placeholder="Last name" required data-parsley-required-message="Please enter a First Name">
                         </div>
                     </div>
                 </div>
                 <div class="mb-3">
-                    <div class="field input">
-                        <label class="form-label">Email Address</label>
-                        <input type="text" name="email" class="form-control" placeholder="Enter your email" required>
-                    </div>
+                    <label for="email" class="form-label">Email Address</label>
+                    <input type="text" name="email" id="email" class="form-control" data-parsley-type="email" placeholder="Enter your email" required data-parsley-required-message="Please enter an Email address">
                 </div>
                 <div class="mb-3">
                     <label for="password" class="form-label">Password</label>
                     <div class="input-group">
-                        <input type="password" name="password" id="password" class="form-control" placeholder="Enter your password">
+                        <input type="password" name="password" id="password" class="form-control" placeholder="Enter your password" data-parsley-minlength="6" required data-parsley-errors-container=".error" data-parsley-required-message="Please enter a Password">
                         <button class="btn btn-outline-secondary" type="button" id="togglePassword">
                             <i class="fas fa-eye"></i>
                         </button>
                     </div>
+                    <div class="error"></div>
                 </div>
                 <div class="mb-3">
                     <label for="cPassword" class="form-label">Confirm Password</label>
                     <div class="input-group">
-                        <input type="password" name="cPassword" id="cPassword" class="form-control" placeholder="Confirm your password">
+                        <input type="password" name="cPassword" id="cPassword" class="form-control" placeholder="Confirm your password" data-parsley-minlength="6" required data-parsley-errors-container=".errorCP" required data-parsley-required-message="Please enter a Confirm Password">
                         <button class="btn btn-outline-secondary" type="button" id="toggleCPassword">
                             <i class="fas fa-eye"></i>
                         </button>
                     </div>
+                    <div class="errorCP"></div>
                 </div>
                 <div class="mb-3">
                     <div class="field image">
                         <label class="form-label">Select Image</label>
-                        <input type="file" name="image" class="form-control" accept="image/x-png,image/gif,image/jpeg,image/jpg" required>
+                        <input type="file" name="image" class="form-control" accept="image/x-png,image/gif,image/jpeg,image/jpg" required data-parsley-required-message="Please choose an image">
                     </div>
                 </div>
                 <div class="mb-3">
@@ -61,8 +61,18 @@
     </div>
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="/mvc architecture/asserts/javascript/parsley.min.js"></script>
+
     <script>
         $(document).ready(function() {
+
+            $("#registerForm").parsley({
+                errorClass: "is-invalid text-danger",
+                successClass: "is-valid",
+                errorsWrapper: '<span class="form-group"></span>',
+                errorTemplate: '<small class="form-text text-danger"></small>'
+            });
+
             $('#togglePassword').click(function() {
                 var passwordInput = $('#password');
                 var passwordFieldType = passwordInput.attr('type');
